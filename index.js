f5cc234502e5e107254599068b1586d8cead2f97
centinela1024tb <centinela1024tb@gmail.com>
2023-07-11 16:47:53 -0600

    [CONFIG] configuración de la conexión a la base de datos y puerto del server
    => se usa variables de entorno para ocultar la uri de conexión

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+import conectarDB from './config/db.js';
 
 const app = express()
 
+// habilitamos las variables de entorno
+dotenv.config();
 
-app.listen(4000, () => {
-  console.log('Server run on port 4000')
+
+// conxion DB
+conectarDB();
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+  console.log(`Server run on port ${PORT}`)
 })
