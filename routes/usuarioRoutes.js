73a7891584e33a9cc87a3104a8a9bbb9e5dcbf4c
centinela1024tb <centinela1024tb@gmail.com>
2023-07-13 15:52:41 -0600

    [ADD] se agrega la route olvide-password/:token para verificar el usuario que solicita restablecer su password

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -11,10 +11,11 @@ const router = express.Router()
 
 
 //* ===== Authenticación, Registro y Confirmación de Usuarios =======
-router.post('/', registrar);                       //? Crea un nuevo usuario
-router.post('/login', autenticar);                 //? login(iniciar sesion)
-router.get('/confirmar/:token', confirmar)         //? confirmar la cuenta via token
-router.post('/olvide-password', olvidePassword)    //? se envia email para restablecer password
+router.post('/', registrar);                          //? Crea un nuevo usuario
+router.post('/login', autenticar);                    //? login(iniciar sesion)
+router.get('/confirmar/:token', confirmar)            //? confirmar la cuenta via token
+router.post('/olvide-password', olvidePassword)       //? se envia email para restablecer password
+router.get('/olvide-password/:token', comprobarToken) //? check token del que solicito restablecer password
 
 
 
