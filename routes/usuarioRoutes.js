ba31375a35328e102963d8ac296daeff7d0ce2e4
centinela1024tb <centinela1024tb@gmail.com>
2023-07-13 15:28:54 -0600

    [ADD] se agrega la route para confirmar yla cuenta por el token  (se envia en la uri: /confirmar/_token_)

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -3,14 +3,16 @@ import express from 'express'
 import { 
    registrar,
    autenticar,
+   confirmar,
 } from '../controllers/usuarioController.js'
 
 const router = express.Router()
 
 
 //* ===== Authenticación, Registro y Confirmación de Usuarios =======
-router.post('/', registrar);              //? Crea un nuevo usuario
-router.post('/login', autenticar);        //? login(iniciar sesion)
+router.post('/', registrar);                 //? Crea un nuevo usuario
+router.post('/login', autenticar);           //? login(iniciar sesion)
+router.get('/confirmar/:token', confirmar)
 
 
 
