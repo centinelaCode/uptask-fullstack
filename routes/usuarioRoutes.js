a96f16ca3f775adc97049d999363118a32e88bad
centinela1024tb <centinela1024tb@gmail.com>
2023-07-13 16:28:37 -0600

    [ADD] se crea route para guardar el nuevo password, validando que el token pasado por uri sea correcto

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -6,17 +6,23 @@ import {
    confirmar,
    olvidePassword,
    comprobarToken,
+   NuevoPassword,
 } from '../controllers/usuarioController.js'
 
 const router = express.Router()
 
 
 //* ===== Authenticación, Registro y Confirmación de Usuarios =======
-router.post('/', registrar);                          //? Crea un nuevo usuario
-router.post('/login', autenticar);                    //? login(iniciar sesion)
-router.get('/confirmar/:token', confirmar)            //? confirmar la cuenta via token
-router.post('/olvide-password', olvidePassword)       //? se envia email para restablecer password
-router.get('/olvide-password/:token', comprobarToken) //? check token del que solicito restablecer password
+router.post('/', registrar);                            //? Crea un nuevo usuario
+router.post('/login', autenticar);                      //? login(iniciar sesion)
+router.get('/confirmar/:token', confirmar)              //? confirmar la cuenta via token
+router.post('/olvide-password', olvidePassword)         //? se envia email para restablecer password
+// router.get('/olvide-password/:token', comprobarToken)   
+// router.post('/olvide-password/:token', NuevoPassword)   
+router.route('/olvide-password/:token')
+   .get(comprobarToken)                                 //? check token del que solicito restablecer password
+   .post(NuevoPassword)                                 //? check token del que solicito restablecer password
+
 
 
 
