c71ad85bd9cc309d7f5d9c8ec47bbad7de3c7296
centinela1024tb <centinela1024tb@gmail.com>
2023-07-13 13:09:52 -0600

    [ADD] se agrega la route para autenticar un usurio  (iniciar sesion)
    => uri: http://localhost:400/api/usuarios/login

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,12 +1,16 @@
 import express from 'express'
 
-import { registrar } from '../controllers/usuarioController.js'
+import { 
+   registrar,
+   autenticar,
+} from '../controllers/usuarioController.js'
 
 const router = express.Router()
 
 
 //* ===== Authenticación, Registro y Confirmación de Usuarios =======
-router.post('/', registrar);   //? Crea un nuevo usuario
+router.post('/', registrar);              //? Crea un nuevo usuario
+router.post('/login', autenticar);        //? login(iniciar sesion)
 
 
 
