eea04136c6dbd515aeac593a86712b7bff8fcb5c
centinela1024tb <centinela1024tb@gmail.com>
2023-07-14 16:08:18 -0600

    [ADD] Se definen todas las routes y controllers para proyectos
    => obtenerProyectos,
    => obtenerProyecto,
    => nuevoProyecto,
    => editarProyecto,
    => eliminarProyecto,
    => agregarColaborador,
    => eliminarColaborador,
    => obtenerTareas,

---
+++ controllers/proyectoController.js
@@ -0,0 +1,51 @@
+
+
+//* ==========> Obtenemo todos los proyectos de usuario <==========
+const obtenerProyectos = async(req, res) => {}
+
+
+
+//* ==========> Obtenemos un proyecto <==========
+const obtenerProyecto = async(req, res) => {}
+
+
+
+//* ==========> Crea un proyecto <==========
+const nuevoProyecto = async(req, res) => {}
+
+
+
+//* ==========> Actualizar un proyecto <==========
+const editarProyecto = async(req, res) => {}
+
+
+
+//* ==========> Eliminar un proyecto <==========
+const eliminarProyecto = async(req, res) => {}
+
+
+
+//* ==========> Agregar Colaborador <==========
+const agregarColaborador = async(req, res) => {}
+
+
+
+//* ==========> Eliminar Colaborador <==========
+const eliminarColaborador = async(req, res) => {}
+
+
+
+//* ==========> Obtener Tareas <==========
+const obtenerTareas = async(req, res) => {}
+
+
+export {
+   obtenerProyectos,
+   obtenerProyecto,
+   nuevoProyecto,
+   editarProyecto,
+   eliminarProyecto,
+   agregarColaborador,
+   eliminarColaborador,
+   obtenerTareas,
+}
