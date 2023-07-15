4c1f765504dce1a979b3a3ddc2a62ab523745673
centinela1024tb <centinela1024tb@gmail.com>
2023-07-15 13:27:53 -0600

    [ADD] se obtiene un proyecto con validaciones
    => se valida que el proyecto exista
    => que pertenezca al usuario auth
    => o que sea colaborador de ese proyecto

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,9 +1,10 @@
+import mongoose from 'mongoose'
 import Proyecto from '../models/Proyecto.js';
 
 
 //* ==========> Obtenemo todos los proyectos de usuario <==========
 const obtenerProyectos = async(req, res) => {
-   const {_id } = req.usuario
+   // const {_id } = req.usuario
 
    // const proyectos = await Proyecto.find({ creador: _id })
    const proyectos = await Proyecto.find().where('creador').equals(req.usuario)
@@ -13,7 +14,26 @@ const obtenerProyectos = async(req, res) => {
 
 
 //* ==========> Obtenemos un proyecto <==========
-const obtenerProyecto = async(req, res) => {}
+const obtenerProyecto = async(req, res) => {
+
+   const { id } = req.params;
+  
+   // Verificamos si existe el proyecto
+   const proyecto = await Proyecto.findById(id)
+   if(!proyecto) {
+      const error = new Error('El Proyecto no existe')         
+      return res.status(404).json({ msg: error.message });
+   }   
+  
+   // Si proyecto si existe validamos que el proyecto pertenezca al usuario authenticado o es colaborador
+   if(proyecto.creador.toString() !== req.usuario._id.toString()) {
+      const error = new Error('Acción no válida - Acceso denegado')         
+      return res.status(401).json({ msg: error.message });
+   }
+   
+   // si proyecto existe y pertencece al usuario auth o es colaborador
+   res.json(proyecto);   
+}
 
 
 
