770c9c6be64c03812720dc11a3a4286c622d6dfb
centinela1024tb <centinela1024tb@gmail.com>
2023-07-17 17:11:13 -0600

    [ADD] se implementa tareaController y tareaRoutes con las operaciones del CRUD

---
+++ controllers/tareaController.js
@@ -0,0 +1,34 @@
+
+
+
+//* ==========> Crea Tarea <==========
+const crearTarea = async(req, res) => {}
+
+
+
+//* ==========> Obtener Tarea <==========
+const obtenerTarea = async(req, res) => {}
+
+
+
+//* ==========> Actualizar Tarea <==========
+const actualizarTarea = async(req, res) => {}
+
+
+
+//* ==========> Eliminar Tarea <==========
+const eliminarTarea = async(req, res) => {}
+
+
+
+//* ==========> Cambiar Estado de una Tarea <==========
+const cambiarEstado = async(req, res) => {}
+
+
+export {
+   crearTarea,
+   obtenerTarea,
+   actualizarTarea,
+   eliminarTarea,
+   cambiarEstado,
+}
