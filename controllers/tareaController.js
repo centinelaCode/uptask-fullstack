95ba45d95fef2ed032a219c6e099de7aba7a1b17
centinela1024tb <centinela1024tb@gmail.com>
2023-07-18 15:35:44 -0600

    [ADD] se implementa el actualizar una tarea de un proyecto
    => se valida que la tarea exista y que pertenezca al usuario logueado

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -58,7 +58,37 @@ const obtenerTarea = async(req, res) => {
 
 
 //* ==========> Actualizar Tarea <==========
-const actualizarTarea = async(req, res) => {}
+const actualizarTarea = async(req, res) => {
+   console.log('Hola')
+   const { id } = req.params
+
+   // verificanmos que la tarea exista
+   const tarea = await Tarea.findById(id).populate('proyecto')   
+
+   // validamos si existe la tarea
+   if(!tarea) {
+      const error = new Error('Tarea no encontrada')         
+      return res.status(404).json({ msg: error.message });
+   }
+
+   // validamos que el proyecto pertenezca al usuario authenticado o es colaborador
+   if(tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+      const error = new Error('Acción no válida - Acceso denegado')         
+      return res.status(403).json({ msg: error.message });
+   }
+
+   tarea.nombre = req.body.nombre || tarea.nombre;
+   tarea.descripcion = req.body.descripcion || tarea.descripcion;
+   tarea.prioridad = req.body.prioridad || tarea.prioridad;
+   tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega;
+
+   try {
+      const tareaAlmacenada = await tarea.save();
+      res.json(tareaAlmacenada);
+   } catch (error) {
+      console.log(error)
+   }
+}
 
 
 
